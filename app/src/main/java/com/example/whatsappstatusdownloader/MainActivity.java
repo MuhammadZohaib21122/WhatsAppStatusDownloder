ee7c66f753989fe4065ee17a526bad01a224e3f4
MUHAMMAD ZOHAIB <muhammadzohaib21122@gmail.com>
2024-06-07 14:00:45 +0500

    implement fetch status on whatsapp

--- app/src/main/java/com/example/whatsappstatusdownloader/MainActivity.java
+++ app/src/main/java/com/example/whatsappstatusdownloader/MainActivity.java
@@ -1,25 +1,140 @@
 package com.example.whatsappstatusdownloader;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.DocumentsContract;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     SwipeRefreshLayout swipeRefreshLayout;
+    StoryAdapter storyAdapter;
+    File[] files;
+    ArrayList<StoryModel> filesList = new ArrayList<>();
+    private static final String PREFS_NAME = "MyAppPreferences";
+    private static final String KEY_FIRST_LAUNCH = "firstLaunch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        if (isFirstLaunch()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                checkWhatsAppPermission();
+            }
+            markFirstLaunch();
+        }
+
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        setUpRefreshLayout();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    private void markFirstLaunch() {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(KEY_FIRST_LAUNCH, false);
+        editor.apply();
+    }
+
+    private boolean isFirstLaunch() {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return preferences.getBoolean(KEY_FIRST_LAUNCH, true);
     }
 
     private void initViews() {
         recyclerView = findViewById(R.id.recycler_view);
         swipeRefreshLayout = findViewById(R.id.swipeRecyclerView);
+
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            swipeRefreshLayout.setRefreshing(true);
+            setUpRefreshLayout();
+            new Handler().postDelayed(() -> {
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(MainActivity.this, "Refreshed", Toast.LENGTH_SHORT).show();
+            }, 2000);
+        });
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void setUpRefreshLayout() {
+        filesList.clear();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        storyAdapter = new StoryAdapter(MainActivity.this, getData());
+        recyclerView.setAdapter(storyAdapter);
+        storyAdapter.notifyDataSetChanged();
+    }
+
+    private ArrayList<StoryModel> getData() {
+        String targetPath = Environment.getExternalStorageDirectory().getAbsolutePath() + Constant.FOLDER_NAME + "Media/.Statuses";
+        File targetDirector = new File(targetPath);
+
+        if (!targetDirector.exists()) {
+            targetPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/media/com.whatsapp/WhatsApp/Media/.Statuses";
+            targetDirector = new File(targetPath);
+        }
+
+        if (targetDirector.exists() && targetDirector.isDirectory()) {
+            files = targetDirector.listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    if (!file.getName().endsWith(".nomedia")) {
+                        StoryModel story = new StoryModel();
+                        story.setUri(Uri.fromFile(file));
+                        story.setPath(file.getAbsolutePath());
+                        story.setFilename(file.getName());
+                        filesList.add(story);
+                    }
+                }
+            }
+        }
+
+        Toast.makeText(this, "Size = " + filesList.size(), Toast.LENGTH_SHORT).show();
+        return filesList;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void checkWhatsAppPermission() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        Uri wa_status_uri = Uri.parse("content://com.android.externalstorage.documents/tree/primary%3AAndroid%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia%2F.Statuses");
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, wa_status_uri);
+        startActivityForResult(intent, 10001);
     }
 }
--- app/src/main/java/com/example/whatsappstatusdownloader/StoryAdapter.java
+++ app/src/main/java/com/example/whatsappstatusdownloader/StoryAdapter.java
@@ -1,22 +1,35 @@
 package com.example.whatsappstatusdownloader;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHolder>{
+public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHolder> {
 
-    private Context context;
-    private ArrayList<Object> filesList;
+    private final Context context;
+    private final ArrayList<StoryModel> filesList;
 
-    public StoryAdapter(Context context, ArrayList<Object> filesList) {
+    public StoryAdapter(Context context, ArrayList<StoryModel> filesList) {
         this.context = context;
         this.filesList = filesList;
     }
@@ -24,24 +37,80 @@ public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.StoryViewHol
     @NonNull
     @Override
     public StoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.card_row,null,false);
+        @SuppressLint("InflateParams") View view = LayoutInflater.from(context).inflate(R.layout.card_row, parent, false);
         return new StoryViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StoryViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull StoryViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        final StoryModel file = filesList.get(position);
 
+        if (file.getUri().toString().endsWith(".mp4")) {
+            holder.playIcon.setVisibility(View.VISIBLE);
+        } else {
+            holder.playIcon.setVisibility(View.INVISIBLE);
+        }
 
+        Glide.with(context)
+                .load(file.getUri())
+                .into(holder.saveImage);
+
+        holder.downloadID.setOnClickListener(v -> {
+            checkFolder();
+
+            final String path = file.getPath();
+            final File sourceFile = new File(path);
+
+            String destPath = Environment.getExternalStorageDirectory().getAbsolutePath() + Constant.SAVE_FOLDER_NAME;
+            File destFile = new File(destPath);
+
+            try {
+                FileUtils.copyFileToDirectory(sourceFile, destFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            MediaScannerConnection.scanFile(
+                    context,
+                    new String[]{destPath + file.getFilename()},
+                    new String[]{"*/*"},
+                    new MediaScannerConnection.MediaScannerConnectionClient() {
+                        @Override
+                        public void onMediaScannerConnected() {
+                        }
+
+                        @Override
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    }
+            );
+
+            Toast.makeText(context, "Saved to:" + destPath + file.getFilename(), Toast.LENGTH_SHORT).show();
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return filesList.size();
     }
 
-    public class StoryViewHolder extends RecyclerView.ViewHolder{
+    private void checkFolder() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + Constant.SAVE_FOLDER_NAME;
+        File dir = new File(path);
 
-        ImageView playIcon,downloadID,saveImage;
+        if (!dir.exists()) {
+            boolean isDirectoryCreated = dir.mkdirs();
+            if (isDirectoryCreated) {
+                Log.d("Folder", "Created Successfully");
+            }
+        } else {
+            Log.d("Folder", "Already Exists");
+        }
+    }
+
+    public static class StoryViewHolder extends RecyclerView.ViewHolder {
+        ImageView playIcon, downloadID, saveImage;
+
         public StoryViewHolder(@NonNull View itemView) {
             super(itemView);
             saveImage = itemView.findViewById(R.id.mainImageView);
